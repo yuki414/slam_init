48f08e3cdb92a9c9fc3ca7eb4ec8c5db288d6de7
yuki414 <q42ghj8cfr@gmail.com>
2019-04-10 17:50:11 +0900

    fix part of arguments

--- MapDrawer.py
+++ MapDrawer.py
@@ -1,6 +1,6 @@
 '''
 マップ描画のところ
-
+C++のgpみたいなのをつかわないでかけないのか？
 '''
 
 class MapDrawer:
@@ -37,3 +37,49 @@ class MapDrawer:
         self.ymax = yM
         print("set xrange [%f:%f]" % (self.xmin, self.xmax))
         print("set yrange [%f:%f]" % (self.ymin, self.ymax))
+
+    def drawMapGp(pcmap):
+        lps = pcmap.globalMap()
+        poses = pcmap.poses()
+        drawGp(lps, poses)
+
+    def drawScanGp(scan):
+        poses = Pose2D()
+        pose = Pose2D()
+        poses.emplace_back(pose)
+        drawGp(scan.lps, poses)
+
+    def drawTrajectoryGp(poses):
+        lps = LPoint2D()
+        drawGp(lps, poses)
+
+    def drawGp(lps, poses, flus):
+        print("drawGp: lps.size={}".format(len(lps))
+
+          # // gnuplot設定
+          # fprintf(gp, "set multiplot\n");
+          # //  fprintf(gp, "plot '-' w p pt 7 ps 0.1, '-' with vector\n");
+          # fprintf(gp, "plot '-' w p pt 7 ps 0.1 lc rgb 0x0, '-' with vector\n");
+        step1 = 1
+        # 描画の書き方
+        # range(start, stop, step)
+        # for i in range(0, len(lps), step1):
+
+        step2 = 10
+        for i in range(0, len(lps), step2):
+            cx = pose.tx
+            cy = pose.ty
+            cs = pose.Rmat[0][0]
+            sn = pose.Rmat[1][0]
+
+            dd = 0.4
+            x1 = cs*dd
+            y1 = sn*dd
+            x2 = -sn*dd
+            y2 = cs*dd
+
+            print("{} {} {} {}" .format(cx, cy, x1, y1))
+            print("{} {} {} {}" .format(cx, cy, x2, y2))
+
+        # if (flush):
+            #
--- main.py
+++ main.py
@@ -10,78 +10,80 @@ SLAM入門を参考
 
 '''
 # In[]:
+import sys
 import os.path
 from enum import Enum
 from SLAM_launcher import SLAM_launcher
 # ここでエラーがでるが，pose2dなどを定義していないため
-# In[]:
-# あとで入力引数として書き換え
-class main():
-    def __init__(self, argc, argv):
-        # オプションを渡して起動したいときにもちいる
-        # argcはオプションの個数(int)，argvはオプションを格納する(char)
-        self.argc = argc
-        self.argv = argv
 
+# In[]:
+# あとで入力引数として書き換えた
+def main():
+    # オプションを渡して起動したいときにもちいる
+    # argcはオプションの個数(int)，argvはオプションを格納する(char)
+    argv = sys.argv
+    argc = len(argv)
     # 準備
     scanCheck = False  # スキャン表示か
     odometryOnly = False  # オドメトリのみによる地図構築か
     startN = 0
+    # filenameはstr型
     print('hello')
+    if (argc < 2):
+        print('Error: too few arguments.')
+        sys.exit(1)
+
+    # コマンド引数の処理??
+    idx = 1
+    if (argv[1][0] == '-'):
+        for i in range(30):
+            # 原本では終了条件が指定されていない，breakで抜ける
+            # 無限ループは少し怖いので適当にうちきり，あとで治す
+            option = argv[1][i]
+            # 存在しない要素を参照しようとするとエラーがでるのでは？
+            if (option == None):
+                break
+            elif (option == 's'):
+                scanCheck = True
+            elif (option == 'o'):
+                odometryOnly = True
+
+        if (argc == 2):
+            print('Error: no file name.')
+            sys.exit(1)
+
+        idx = idx + 1
+    if (argc >= idx + 1):
+        filename = argv[idx]
+    if (argc == idx + 2):
+        startN = int(argv[idx + 1])  # str to int
+    elif (argc >= idx + 2):
+        print('Error: invalid arguments.')
+        sys.exit(1)
+    print('SlamLauncher: startN=%d, scanCheck=%d, odometryOnly=%d' % (startN, scanCheck, odometryOnly))
+    print('filename={}'.format(filename))
 # In[]:
-    # main関数のメインの部分
-    def excute(self):
-        if self.argc < 2:
-            print('Error: too few arguments.')
-            exit()  # !!注意: return 1に変わる関数がわからない
+    sl = SlamLauncher()
+    # sl = SLAMLauncher(_startN, _odometryOnly) # __init__のための引数を入れる場合こっち
 
-        # コマンド引数の処理??
-        idx = 1
-        if (self.argv[1, 0] == '-'):
-            for i in range(30):
-                # 原本では終了条件が指定されていない，breakで抜ける
-                # 無限ループは少し怖いので適当にうちきり，あとで治す
-                option = argv[1, i]
-                if (option == None):
-                    break
-                elif (option == 's'):
-                    scanCheck = True
-                elif (option == 'o'):
-                    odometryOnly = True
-            if (argc == 2):
-                print('Error: no file name.')
-                exit()  # !!注意
+    # filenameを渡し，中でfilepathがあるか確認する
+    # 存在する場合trueを返し，しない場合エラー表示とfalseを返す
+    flag = sl.setFilename(filename)
+    if not(flag):
+        sys.exit()
 
-            idx = idx + 1
-        if (argc >= idx + 1):
-            filename = argv[idx]
-        if (argc == idx + 2):
-            startN = atoi(argv[idx + 1])  # !!注意: atoi，急に出てきた
-        elif (argc >= idx + 2):
-            print('Error: invalid arguments.')
-            exit()
+    sl.setStartN(startN)
+
+    # 処理本体
+    if (scanCheck):
+        sl.showScans()
+    else:
+        sl.setOdometryOnly(odometryOnly)
+        sl.customizeFramework()
+        sl.run()
+
+    return 0  # 問題なくおわったときのCの処理だけど
+    # pythonでreturnいれないで処理するとnoneが帰ってきて別に何もいらないときはどうすればよいのだろう
 # In[]:
-    # ファイルを開く
-    def open_file(self):
-        # slamランチャーの起動，現時点では存在しないためコメントアウト
-        # SLAM_launcher sl
-        sl = SLAM_launcher(_startN, _odometryOnly) # __init__のための引数をいれろ
-        flag = sl.setFilename(filename)  # 原本では bool flagなので，True or False
-        if flag != True:  # !flag = not false = true で実行
-            exit()
-
-        sl.setStartN(startN)
-
-        # 処理本体
-        if scanCheck == True:
-            sl.showScans()
-        else:
-            sl.setOdometryOnly(odometryOnly)
-            sl.customizeFramework()
-            sl.run()
-
-        return 0  # 問題なくおわったときのCの処理だけど
-        # pythonでreturnいれないで処理するとnoneが帰ってきて別に何もいらないときはどうすればよいのだろう
-# In[]:
-5+2
-print('Test!')
+if __name__ == '__main__':
+    main()
