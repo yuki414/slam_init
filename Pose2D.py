bff9e5a5461a8c26792644f6441f2f55b79d6966
yuki414 <q42ghj8cfr@gmail.com>
2019-04-23 18:17:12 +0900

    add part of pose method

--- Pose2D.py
+++ Pose2D.py
@@ -48,8 +48,80 @@ class Pose2D():
     def setAngle(self, th):
         self.th = th
 
+    def relativePoint(self, _p):
+        # p = LPoint2D()で与えられている
+        self.dx = _p.x - self.tx
+        self.dy = _p.y - self.ty
+        self.x = self.dx*self.Rmat[0][0] + self.dy*self.Rmat[1][0]
+        self.y = self.dx*self.Rmat[1][0] + self.dy*self.Rmat[1][1]
+        return LPoint2D(p.sid, x, y) # !!:return LPoint2dの意味がよくわかないため放置
+
+    # 自分（Pose2D）の局所座標系での点pを、グローバル座標系に変換
+    def globalPoint_single(self, _p):
+        self.x = self.Rmat[0][0]*_p.x + self.Rmat[0][1]*_p.y + self.tx
+        self.y = self.Rmat[1][0]*_p.x + self.Rmat[1][1]*_p.y + self.ty
+        return LPoint2D(p.sid, x, y)
+
+    # 自分（Pose2D）の局所座標系での点pを、グローバル座標系に変換してpoに入れる
+    def globalPoint_double(self, _pi, _po):
+        # pi,po両方LPoint2Dを継承
+        _po.x = self.Rmat[0][0]*_pi.x + self.Rmat[0][1]*_pi.y + tx
+        _po.y = self.Rmat[1][0]*_pi.x + self.Rmat[1][1]*_pi.y + ty
+        # このメソッドはvoidのため返り値なし．ただしpi,poはポインタを指してるためpoは変化があるはず
+
+    # 基準座標系bposeから見た現座標系nposeの相対位置relPoseを求める（Inverse compounding operator）
+    def calRelativePose(self, _npose, _bpose, _relPose):
+        # ここでしか使わない変数だからインスタンスに含めなくてもいいかも？
+        # と思ったがそもそもR1,R2はここでも使われてないからどうしていいかわからん
+        self.R0 = _bpose.Rmat
+        self.R1 = _npose.Rmat
+        self.R2 = _relPose.Rmat
+
+        # ソースコードはこれ意味わからん
+        # const double (*R0)[2] = bpose.Rmat;           // 基準座標系
+        # const double (*R1)[2] = npose.Rmat;           // 現座標系
+        # double (*R2)[2] = relPose.Rmat;               // 相対位置
+
+        # 並進
+        self.dx = _npose.tx - _bpose.tx
+        self.dy = _npose.ty - _bpose.ty
+        _relPose.tx = self.R0[0][0]*self.dx + self.R0[1][0]*self.dy
+        _relPose.ty = self.R0[0][1]*self.dx + self.R0[1][1]*self.dy
+
+        # 回転
+        self.th = _npose.th - _bpose.th
+        if (self.th< -180):
+            self.th = self.th + 360
+        elif (self.th >= 180):
+            self.th = self.th - 360
+        _relPose.th = self.th # !!:relはポインタが指定されてるのでこれをしても意味がないのでは
+        _relPose.calRmat()
+
+    # 基準座標系bposeから相対位置relPoseだけ進んだ、座標系nposeを求める（Compounding operator）
+    def calGlobalPose(self, _relPose, _bpose, _npose):
+        self.R0 = _bpose.Rmat
+        self.R1 = _relPose.Rmat
+        self.R2 = _npose.Rmat
+
+        # 並進
+        self.tx = _relPose.tx
+        self.ty = _relPose.ty
+        _npose.tx = self.R0[0][0]*self.tx + self.R0[0][1]*self.ty + _bpose.tx
+        _npose.ty = self.R0[1][0]*self.tx + self.R0[1][1]*self.ty + _bpose.ty
+
+        # 角度
+        self.th = _bpose.th + _relPose.th
+        if (self.th < -180):
+            self.th = self.th + 360
+        elif (self.th >= 180):
+            self.th = self.th - 360
+        _npose.th = th # 上述同様
+        _npose.calRmat()
+
+
     '''
     ワケワカランポイント
+    と思ったらPose2D.cppを見てなかったためそれのせいかも
     LPoint2D relativePoint(const LPoint2D &p) const;
     LPoint2D globalPoint(const LPoint2D &p) const;
     void globalPoint(const LPoint2D &pi, LPoint2D &po) const;
